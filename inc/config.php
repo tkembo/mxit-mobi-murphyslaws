01f8995a75e68f1441770898bd004aba7ee6752e
Tawanda Kembo <tkembo@gmail.com>
2013-04-29 14:57:31 +0200

    changed database settings, shinka id & analytics id

--- inc/config.php
+++ inc/config.php
@@ -1,20 +1,20 @@
 <?php
 	
 	//Configuration for Shinka Banners
-	$config = array('AdUnitID_320'  => '35XXXX',
+	$config = array('AdUnitID_320'  => '352558',
                     'RESIZE_IMAGES' => TRUE,
-                    'REFERER'       => 'XXXX',
-                    'TESTUSER'      => 'XXXX');
+                    'REFERER'       => 'murphyslaws',
+                    'TESTUSER'      => 'tkembo');
 	
 	//Configuration for Google Analytics
-	$GA_ACCOUNT = "MO-XXXXXXXX-X";
+	$GA_ACCOUNT = "MO-28800751-1";
 	$GA_PIXEL = "ga.php";
 	
 	//DB Settings
-	$hostname_mxit_murphyslaws = "XXXXXXXXXXXXX.XXXXXXXXXXXX.us-east-1.rds.amazonaws.com";
-	$database_mxit_murphyslaws = "mxit_murphyslaws";
-	$username_mxit_murphyslaws = "XXXXXX";
-	$password_mxit_murphyslaws = "XXXXXX";
+	$hostname_mxit_murphyslaws = "us-cdbr-azure-west-b.cleardb.com";
+	$database_mxit_murphyslaws = "mxitmurphyslaws";
+	$username_mxit_murphyslaws = "bf972afd92da56";
+	$password_mxit_murphyslaws = "bc3d7eca";
 	$mxit_murphyslaws = mysql_pconnect($hostname_mxit_murphyslaws, $username_mxit_murphyslaws, $password_mxit_murphyslaws) or trigger_error(mysql_error(),E_USER_ERROR); 
 	
 ?>
