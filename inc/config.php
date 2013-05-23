c06a0083a0f4a14fd3325cb7a2ed67ac00b12302
Tawanda Kembo <tkembo@gmail.com>
2013-05-23 11:32:40 +0200

    now using app variables for db settings

--- inc/config.php
+++ inc/config.php
@@ -11,10 +11,10 @@
 	$GA_PIXEL = "ga.php";
 	
 	//DB Settings
-	$hostname_mxit_murphyslaws = "localhost";
-	$database_mxit_murphyslaws = "mxi_tmurphyslaws";
-	$username_mxit_murphyslaws = "root";
-	$password_mxit_murphyslaws = "";
+	$hostname_mxit_murphyslaws = getenv('DB_HOST');
+	$database_mxit_murphyslaws = getenv('DB_NAME');
+	$username_mxit_murphyslaws = getenv('DB_USER');
+	$password_mxit_murphyslaws = getenv('DB_PASS');
 	
 	$mxit_murphyslaws = mysql_pconnect($hostname_mxit_murphyslaws, $username_mxit_murphyslaws, $password_mxit_murphyslaws) or trigger_error(mysql_error(),E_USER_ERROR); 
 	
